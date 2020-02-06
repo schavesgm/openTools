3b6f8836e64d1058dd85f9ccebbc858a1d7c61c0
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2020-02-06 10:49:00 +0000

    Formatting in comments

--- pythonFitCodes/fitCode/fitFunctionNM.py
+++ pythonFitCodes/fitCode/fitFunctionNM.py
@@ -6,13 +6,18 @@ __all__ = ['fitOnceNM', 'fitNtimes']
 
 def chiSquare(f, args, xData, yData, yError = 0 ):
     '''
-        @arg f (function) : function that we would like to fit with the data. Must return a double
-                            and be able to accept numpy arrays as input
+        @arg f (function) : function that we would like to fit with the data.
+                            Must return a double and be able to accept numpy
+                            arrays as input
         @arg args (array) : values that we would like to fit
-        @arg xData(array) : xData extracted from the experiment that we would like to fit
-        @arg yData(array) : yData extracted from the experiment that we would like to fit
-        @arg yError(array): error in the measured function used to calculate the fit function
-        return            : double with the chiSquare value at the current status of the coeffs
+        @arg xData(array) : xData extracted from the experiment that we would
+                            like to fit
+        @arg yData(array) : yData extracted from the experiment that we would
+                            like to fit
+        @arg yError(array): error in the measured function used to calculate the
+                            fit function
+        return            : double with the chiSquare value at the current
+                            status of the coeffs
     '''
     if yError.all() == 0:
         return np.sum( ( f(args, xData) - yData ) ** 2 ) / len(xData)
@@ -23,12 +28,16 @@ def chiSquare(f, args, xData, yData, yError = 0 ):
 def fitOnceNM( f, initGuess, xData, yData, yError = 0,
                seed = 1231523, maxIter = 1e6, getChiSquare = 0 ):
     '''
-        @arg f (function) : function that we would like to optimize. This function must return a
-                            double and be able to get numpy arrays as input
+        @arg f (function) : function that we would like to optimize. This
+                            function must return a double and be able to get
+                            numpy arrays as input
 
-        @initGuess (array): array with the initial guess of the points that minimize the function
-        @maxIter (int)    : integer with the maximum number of iterations that we accept
-        return: tuple     : tuple( Points that minimize the function, minimum value achieved)
+        @initGuess (array): array with the initial guess of the points
+                            that minimize the function
+        @maxIter (int)    : integer with the maximum number of iterations
+                            that we accept
+        return: tuple     : tuple( Points that minimize the function,
+                            minimum value achieved)
     '''
 
     np.random.seed( seed )
@@ -43,12 +52,12 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
     noImprovementBreak = 1000
 
     # INIT THE ALGORITHM
-    N = initGuess.shape[0];               ## Dimensions that we are working in
-    evalFunc = chiSquare(f,initGuess,xData,yData,yError)   ## Value of function at the initial guess
+    N = initGuess.shape[0];
+    evalFunc = chiSquare(f,initGuess,xData,yData,yError)
     noImprov = 0
-    imValues = [[initGuess, evalFunc]]        ## Initial values and its images on the function
+    imValues = [[initGuess, evalFunc]]
 
-    for i in range( N ):                ## Generates the initial simplex appending over initGuess
+    for i in range( N ):
         newPoint = cp.copy(initGuess) + initGuess * np.random.random(N)
         imNewPoint= chiSquare(f,newPoint,xData,yData,yError)
         imValues.append([newPoint, imNewPoint])
@@ -58,7 +67,7 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
     while 1:
 
         # ORDER THE EVALUATED FUNCTION
-        imValues.sort( key = lambda x: x[1] )   ## Sort using the evaluated function as axis
+        imValues.sort( key = lambda x: x[1] )   ## Sort using the evaluated function
         lowestValue = imValues[0][1]            ## Best value == lowest value
 
         # BREAK IF MAX ITERATION VALUE IS ACHIEVED
@@ -66,7 +75,8 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
             bestData = imValues[0][0]           ## Best minimum obtained
             evalChiSQ = imValues[0][1]          ## Value of the chiSquare achieved
 
-            if getChiSquare == 0:                ## Control variable to print out the chiSquare
+            if getChiSquare == 0:                ## Control variable to print out
+                                                 ## the chiSquare
                 return bestData
             else:
                 return bestData, evalChiSQ
@@ -87,7 +97,8 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
             bestData = imValues[0][0]           ## Best minimum obtained
             evalChiSQ = imValues[0][1]          ## Value of the chiSquare achieved
 
-            if getChiSquare == 0:                ## Control variable to print out the chiSquare
+            if getChiSquare == 0:                ## Control variable to print
+                                                 ## out the chiSquare
                 return bestData
             else:
                 return bestData, evalChiSQ
@@ -96,51 +107,47 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
         centroidPoint = [0.] * N
 
         for tup in imValues[:-1]:               ## Get all the tuples in the 'matrix'
-            for i, c in enumerate(tup[0]):      ## Disect both tuples, we just need the first one
+            for i, c in enumerate(tup[0]):      ## Disect both tuples, we just need
+                                                ## the first one
                 centroidPoint[i] += c / (len(imValues)-1)       ## Sum the centroid
 
         # REFLECTION PART OF THE ALGORITHM
         xRef = centroidPoint + alpha * ( centroidPoint - imValues[-1][0])
         refEval = chiSquare(f,xRef,xData,yData,yError)
 
-        if imValues[0][1] <= refEval < imValues[-2][1]:    ## If the lowest value is lower than
-                                                           ## the refEval and refEval is lower
-                                                           ## than the second largest value
-
+        if imValues[0][1] <= refEval < imValues[-2][1]:
             del imValues[-1]                               ## Remove the last point
-            imValues.append([xRef, refEval])               ## Append the reflection as highest point
+            imValues.append([xRef, refEval])               ## Append the reflection
+                                                           ## as highest point
             continue
 
         # EXPANSION PART OF THE ALGORITHM
-        if refEval < imValues[0][1]:                       ## If the value of the reflection is
-                                                           ## smaller than the lowest value
+        if refEval < imValues[0][1]:
 
             xExp = centroidPoint + gamma * ( xRef - imValues[-1][0] )
             expValue = chiSquare(f,xExp,xData,yData,yError)
-            if expValue < refEval:                         ## If the value of the expansion is
-                                                           ## less than the reflection value
+            if expValue < refEval:
                 del imValues[-1]
-                imValues.append([xExp, expValue])          ## We append the expansion to the last
-                                                           ## position
+                imValues.append([xExp, expValue])
+
                 continue
             else:
                 del imValues[-1]
-                imValues.append([xRef, refEval])           ## If not, we append the reflection
+                imValues.append([xRef, refEval])
                 continue
 
         # CONTRACTION PART OF THE ALGORITHM
         xCont = centroidPoint + rho * ( centroidPoint - imValues[-1][0] )
         contEval = chiSquare(f,xCont,xData,yData,yError)
 
-        if contEval < imValues[-1][1]:                     ## If the contraction image is smaller
-                                                           ## than the highest point, replace it
+        if contEval < imValues[-1][1]:
+
             del imValues[-1]
             imValues.append([xCont, contEval])
             continue
 
         # REDUCTION PART
-        nimValues = []                                     ## We contract everything using the
-                                                           ## lowest value as the fixed point
+        nimValues = []
 
         for tup in imValues:
             xRed =  imValues[0][0] + sigma * ( tup[0] - imValues[0][0] )
@@ -150,9 +157,9 @@ def fitOnceNM( f, initGuess, xData, yData, yError = 0,
 
 def fitNtimes( f, nReps, initGuess, xData, yData, yError = 0, seed = 1231241 ):
     '''
-        Routine that calls "fitOnceNM" nReps times in order to get some statistics. The value of
-        initGuess is multiplied by some random value in order to randomize the nCalculations and
-        explore the space better.
+        Routine that calls "fitOnceNM" nReps times in order to get some
+        statistics. The value of initGuess is multiplied by some random value
+        in order to randomize the nCalculations and explore the space better.
     '''
 
     getFit = []
