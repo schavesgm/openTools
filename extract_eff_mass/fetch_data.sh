f3908969d10ec57dfaa281a74b86be65da48afb9
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2020-03-23 18:11:09 +0000

    Extended the scripts so that any possible flavor structure and channel can be calculated

--- extract_eff_mass/fetch_data.sh
+++ extract_eff_mass/fetch_data.sh
@@ -1,47 +1,71 @@
 #!/bin/bash
 
 NT=( 128 64 56 48 40 36 32 28 24 20 16 )
-chan="tespConc"
+MESON=( uu us uc ss sc cc )
+
+case $1 in
+    'g5') CHANNEL='Pseudoscalar'; fold='g5';;
+    'vec') CHANNEL='Vector_Spatial'; fold='vecConc';;
+    'ax_plus') CHANNEL='Ax_plus_Spatial'; fold='axplusConc';;
+    'ax_minus') CHANNEL='Ax_minus_Spatial'; fold='axminusConc';;
+    'g0') CHANNEL='Scalar'; fold='g0';;
+    *) echo 'ERROR: Channel provided does not exist!'; exit 1 ;;
+esac
+
+function gen_plot() {
+# Define the variables for the plot file
+chan=$1; meson=$2; nt=$3; sourc=$4; output_file=$5
+
+echo "
+set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2
+
+set style line 11 lc rgb '#808080' lt 1
+set border 3 back ls 11
+set tics nomirror
+
+set style line 12 lc rgb '#808080' lt 0 lw 1
+set grid back ls 12
+
+set xlabel '\$\\tau / a_\\tau\$'
+set ylabel '\$M_{eff} \\cdot a_\\tau \$'
+set title '${chan} -- ${meson} -- ${nt}x32 -- ${type}'
+
+plot '${output_file}' u 1:2:3 w yerr ls 1 notitle
+" > ./plot.gn
+}
+
+SCW="/home/s.989336/Analyse_Corrs/data"
 
 for nt in ${NT[@]}
 do
 
-    mkdir -p ${nt}x32.cc
-    for type in ll ss
+    for meson in ${MESON[@]}
     do
-        # Copy the file into the folder
-        locat="/home/s.989336/Fastsum-Lat/${nt}x32/concMeson.cc/${type}/${chan}_Folder/*"
-        mkdir -p ${nt}x32.cc/${type}
-        scp scw:${locat} ${nt}x32.cc/${type}
+        # Create the folder that will hold the results
+        mkdir -p ${CHANNEL}/${meson}/${nt}x32
+        
+        # Loop over sources ll and ss
+        for type in ll ss
+        do
+            # Copy the file into the folder
+            loc="$SCW/${nt}x32/concMeson.cc/${type}/${fold}_Folder/*"
+            mkdir -p ${CHANNEL}/${meson}/${nt}x32/${type}
+            scp scw:${loc} ${CHANNEL}/${meson}/${nt}x32/${type}
 
         # Then execute the script and plot
-        cp extract_cosh_mass.py ${nt}x32.cc/${type}/
-        cd ${nt}x32.cc/${type}/
+        cp extract_cosh_mass.py \
+            ${CHANNEL}/${meson}/${nt}x32/${type}/
 
-        nameFile=$( ls Gen2l* )
-        fileEff="effMass_${type}_${nameFile}"
+        cd ${CHANNEL}/${meson}/${nt}x32/${type}/
+        name_conf=$( ls Gen2l* )
+        output_file="effMass_${type}_${nameFile}"
 
-        python ./extract_cosh_mass.py ${nameFile} ${fileEff}
-        echo "
+        python ./extract_cosh_mass.py ${name_conf} ${output_file}
 
-        set style line 2 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2 # --- green
-
-        set style line 11 lc rgb '#808080' lt 1
-        set border 3 back ls 11
-        set tics nomirror
-
-        set style line 12 lc rgb '#808080' lt 0 lw 1
-        set grid back ls 12
-
-        set xlabel '\$\\tau / a_\\tau\$'
-        set ylabel '\$M_{eff} \\cdot a_\\tau \$'
-        set title '${chan}.cc -- ${nt}x32 -- ${type}'
-
-        plot '${fileEff}' u 1:2:3 w yerr ls 1 notitle
-        " > ./plot.gn
-
-        gnp2tex plot.gn effMass_${chan}_${nt}x32_${type}.pdf
+        gen_plot $1 $meson $nt $type $output_file
+        gnp2tex plot.gn $1_effMass_${nt}x32_${type}_${meson}.pdf
 
         cd ../../
+        done
     done
 done
