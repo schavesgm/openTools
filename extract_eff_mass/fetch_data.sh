258ca8d1747a605054562916012de0cb56a39621
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2020-03-24 18:33:44 +0000

    Now we label the states by their quantum numbers J^{PC}

--- extract_eff_mass/fetch_data.sh
+++ extract_eff_mass/fetch_data.sh
@@ -4,17 +4,22 @@ NT=( 128 64 56 48 40 36 32 28 24 20 16 )
 MESON=( uu us uc ss sc cc )
 
 case $1 in
-    'g5') CHANNEL='Pseudoscalar'; fold='g5';;
-    'vec') CHANNEL='Vector_Spatial'; fold='vecConc';;
-    'ax_plus') CHANNEL='Ax_plus_Spatial'; fold='axplusConc';;
-    'ax_minus') CHANNEL='Ax_minus_Spatial'; fold='axminusConc';;
-    'g0') CHANNEL='Scalar'; fold='g0';;
+    'g5') CHANNEL='Pseudoscalar'; fold='g5'; 
+        state='0^{+-}' ;;
+    'vec') CHANNEL='Vector_Spatial'; fold='vecConc'; 
+        state='1^{--}' ;;
+    'ax_plus') CHANNEL='Ax_plus_Spatial'; fold='axplusConc'; 
+        state='1^{++}' ;;
+    'ax_minus') CHANNEL='Ax_minus_Spatial'; fold='axminusConc';
+        state='1^{+-}' ;;
+    'g0') CHANNEL='Scalar'; fold='g0';
+        state='0^{++}' ;;
     *) echo 'ERROR: Channel provided does not exist!'; exit 1 ;;
 esac
 
 function gen_plot() {
 # Define the variables for the plot file
-chan=$1; meson=$2; nt=$3; sourc=$4; output_file=$5
+state=$1; meson=$2; nt=$3; sourc=$4; output_file=$5
 
 echo "
 set style line 1 lc rgb '#5e9c36' pt 6 ps 1 lt 1 lw 2
@@ -28,7 +33,7 @@ set grid back ls 12
 
 set xlabel '\$\\tau / a_\\tau\$'
 set ylabel '\$M_{eff} \\cdot a_\\tau \$'
-set title '${chan} -- ${meson} -- ${nt}x32 -- ${type}'
+set title '\$${state} -- ${meson} -- ${nt}x32 -- ${type}\$'
 
 plot '${output_file}' u 1:2:3 w yerr ls 1 notitle
 " > ./plot.gn
@@ -66,7 +71,7 @@ do
 
         python ./extract_cosh_mass.py ${name_conf} ${output_file}
 
-        gen_plot $1 $meson $nt $type $output_file
+        gen_plot $state $meson $nt $type $output_file
         gnp2tex -f plot.gn -s $1_effMass_${nt}x32_${type}_${meson}.pdf
 
         # Remove unneeded files
